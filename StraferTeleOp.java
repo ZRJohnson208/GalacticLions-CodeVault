d54ffc77d6289c16c9afa064d7368561d11081e7
ZRJohnson208 <zachrjohnson08@gmail.com>
2025-08-27 20:56:46 -0400

    Update StraferTeleOp.java

--- StraferTeleOp.java
+++ StraferTeleOp.java
@@ -1,12 +1,11 @@
 package org.firstinspires.ftc.team18443;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
